25cce2e10797d4107ad27ebd596ac9c03a0c8fa2
Sam <98478965+ydnah@users.noreply.github.com>
2024-05-23 12:21:41 +0100

    Creating database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class SearchLog(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    CreditScore = models.IntegerField()
+    Geography = models.IntegerField()
+    Gender = models.IntegerField()
+    Age = models.IntegerField()
+    Tenure = models.IntegerField()
+    Balance = models.FloatField()
+    NumOfProductsts = models.IntegerField()
+    HasCrCard = models.IntegerField()
+    IsActiveMember = models.IntegerField()
+    EstimatedSalary = models.FloatField()
+    searchTime = models.DateTimeField(auto_now=True)
