1946d56034a56731e6af116605a26ac56232d6fd
Sam <98478965+ydnah@users.noreply.github.com>
2024-05-26 18:08:10 +0100

    test 2

--- main/views.py
+++ main/views.py
@@ -2,16 +2,16 @@ from django.http import JsonResponse
 from django.db import transaction
 import json
 import pandas as pd
-# import torch
-# import torch.nn as nn
-# from torchvision import models, transforms
-# from PIL import Image
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+from PIL import Image
 import numpy as np
 import pickle
 
 # from .models import SearchLog
-# from flower_dict import class_to_flower
-# from torchvision.models import resnet50, ResNet50_Weights
+from flower_dict import class_to_flower
+from torchvision.models import resnet50, ResNet50_Weights
 
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -20,24 +20,24 @@ with open('trained_models/bankchurn.pkl', 'rb') as f:
     model_churn = pickle.load(f)
 
 # Load Image Classification Model
-# def load_model_oxford102():
-#     model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
-#     model.fc = nn.Linear(model.fc.in_features, 1000)
-#     model.load_state_dict(torch.load('trained_models/oxford102flowers.pth', map_location=torch.device('cpu')))
-#     model.eval()
-#     return model
+def load_model_oxford102():
+    model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
+    model.fc = nn.Linear(model.fc.in_features, 1000)
+    model.load_state_dict(torch.load('trained_models/oxford102flowers.pth', map_location=torch.device('cpu')))
+    model.eval()
+    return model
 
-# model_oxford102 = load_model_oxford102()
+model_oxford102 = load_model_oxford102()
 
-# # Initialize new model with adjusted weights
-# def initialize_new_model(model):
-#     new_model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
-#     new_model.fc = nn.Linear(model.fc.in_features, 102)
-#     new_model.fc.weight.data = model.fc.weight.data[:102]
-#     new_model.fc.bias.data = model.fc.bias.data[:102]
-#     return new_model
+# Initialize new model with adjusted weights
+def initialize_new_model(model):
+    new_model = models.resnet50(weights=ResNet50_Weights.DEFAULT)
+    new_model.fc = nn.Linear(model.fc.in_features, 102)
+    new_model.fc.weight.data = model.fc.weight.data[:102]
+    new_model.fc.bias.data = model.fc.bias.data[:102]
+    return new_model
 
-# new_model_oxford102 = initialize_new_model(model_oxford102)
+new_model_oxford102 = initialize_new_model(model_oxford102)
 
 # Create search log
 # def create_search_log(data):
@@ -75,27 +75,27 @@ def score_json(request):
         return JsonResponse({'error': 'Method not allowed'}, status=405)
 
 # Image classification
-# def classify_image(request):
-#     if request.method == 'POST':
-#         try:
-#             image_path = request.FILES["image"]
-#             image = Image.open(image_path)
-#             preprocess = transforms.Compose([
-#                 transforms.Resize(256),
-#                 transforms.CenterCrop(224),
-#                 transforms.ToTensor(),
-#                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-#             ])
-#             input_tensor = preprocess(image).unsqueeze(0)
-#             with torch.no_grad():
-#                 output = model_oxford102(input_tensor)
-#             _, predicted_class = output.max(1)
-#             predicted_class_name = class_to_flower[predicted_class.item()]
-#             return JsonResponse({'predicted class': predicted_class_name})
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=400)
-#     else:
-#         return JsonResponse({'error': 'Method not allowed'}, status=405)
+def classify_image(request):
+    if request.method == 'POST':
+        try:
+            image_path = request.FILES["image"]
+            image = Image.open(image_path)
+            preprocess = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+            input_tensor = preprocess(image).unsqueeze(0)
+            with torch.no_grad():
+                output = model_oxford102(input_tensor)
+            _, predicted_class = output.max(1)
+            predicted_class_name = class_to_flower[predicted_class.item()]
+            return JsonResponse({'predicted class': predicted_class_name})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
     
 # def returnData(request):
 #     if request.method == 'GET':
