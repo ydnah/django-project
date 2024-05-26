8e939b652b2f143b9f307493e4c62fa5739b5edd
Sam <98478965+ydnah@users.noreply.github.com>
2024-05-26 16:25:09 +0100

    Removing database

--- projectBackend/settings.py
+++ projectBackend/settings.py
@@ -81,16 +81,7 @@ WSGI_APPLICATION = 'projectBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'churn',
-        'USER': 'project',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost', 
-        'PORT': '3306',       
-    }
-}
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
